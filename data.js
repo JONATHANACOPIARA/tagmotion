ad23bf2b4b2e56de5e3b6880f66036e1315041a7
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2025-12-18 10:58:43 -0300

    Refactor data structure in data.js

--- data.js
+++ data.js
@@ -1,24 +1,22 @@
 const data = {
-nome: "Jonathan Guilherme",
-empresa: "CEO da Empresa",
-foto: "https://i.imgur.com/xxxxx.jpg",
-whatsapp: "5588997842847",
-email: "studiojonathanguillherme@gmail.com",
-site: "https://www.instagram.com/jonathanguilhermejg",
-pix: "00975762397",
+  nome: "Jonathan Guilherme",
+  empresa: "CEO da Empresa",
+  foto: "https://i.imgur.com/xxxxx.jpg",
+  whatsapp: "5588997842847",
+  email: "studiojonathanguillherme@gmail.com",
+  site: "https://www.instagram.com/jonathanguilhermejg",
+  pix: "00975762397",
 
+  cardapio: {
+    estabelecimento: "Restaurante Exemplo",
+    imagem: "https://i.imgur.com/yyyyy.jpg",
+    itens: [
+      { nome: "Pizza Calabresa", preco: "R$ 35" },
+      { nome: "Pizza Frango", preco: "R$ 38" }
+    ]
+  },
 
-cardapio: {
-estabelecimento: "Restaurante Exemplo",
-imagem: "https://i.imgur.com/yyyyy.jpg",
-itens: [
-{ nome: "Pizza Calabresa", preco: "R$ 35" },
-{ nome: "Pizza Frango", preco: "R$ 38" }
-]
-},
-
-
-avaliacoes: {
-google: "https://share.google/xxxxx"
-}
+  avaliacoes: {
+    google: "https://share.google/xxxxx"
+  }
 };
