aa4391fc3a5c1dbe935b67c006d1fb6f4328fc00
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2025-12-19 13:01:40 -0300

    Update data.js with new user and menu details

--- data.js
+++ data.js
@@ -1,17 +1,26 @@
 const data = {
-  nome: "",
-  empresa: "",
-  foto: "",
-  whatsapp: "",
-  email: "",
-  site: "",
-  pix: "",
+  nome: "Jonathan Guilherme Oliveira",
+  empresa: "CEO da Empresa",
+  foto: "https://i.imgur.com/e1g5ycm.jpeg",
+  whatsapp: "5588997842847",
+  email: "criativaaudiovisual@gmail.com",
+  site: "https://www.instagram.com/jonathanguilhermejg",
+  pix: "88997842847",
+  wifi: {
+    ssid: "brisa-1768521",
+    senha: "q79ugxxy",
+    tipo: "WPA"
+  },
 
   cardapio: {
-    estabelecimento: "",
-    imagem: "",
+    estabelecimento: "Tag Motion",
+    imagem: "https://i.imgur.com/7noZxtR.png",
     itens: [
-      
+      { nome: "Cartão de Visita Padrão", preco: "R$ 120,00" },
+      { nome: "Cartão de Visita + Avaliações", preco: "R$ 180,00" },
+      { nome: "Cardápio com Aproximação", preco: "200,00" },
+      { nome: "COMBO PREMIUM", preco: "" },
+      { nome: "CARTÃO", preco: "GOOGLE AVALIAÇÕES" }
     ]
   },
 
