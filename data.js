751089bec84d328cb6295f4808832ed86c1df5be
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2025-12-19 17:20:06 -0300

    Update data object with new company information

--- data.js
+++ data.js
@@ -1,28 +1,26 @@
 const data = {
-  nome: "Jonathan Guilherme Oliveira",
-  empresa: "CEO da Empresa",
-  foto: "https://i.imgur.com/e1g5ycm.jpeg",
-  whatsapp: "5588997842847",
-  email: "studiojonathanguilherme@gmail.com",
-  site: "https://www.instagram.com/jonathanguilhermejg",
-  pix: "88997842847",
+  nome: "Empresa Teste",
+  empresa: "Tag Motion NFC",
+  foto: "https://via.placeholder.com/300",
+
+  whatsapp: "5588999999999",
+  email: "contato@empresa.com",
+  site: "https://google.com",
+
+  pix: "123e4567-e89b-12d3-a456-426614174000",
+
   wifi: {
-    ssid: "brisa-1768521",
-    senha: "q79ugxxy",
-    tipo: "WPA"
+    tipo: "WPA",
+    ssid: "WiFi Empresa",
+    senha: "12345678"
   },
 
   cardapio: {
-    estabelecimento: "Studio JG",
-    imagem: "https://i.imgur.com/umvNas6.jpeg",
+    estabelecimento: "Cardápio",
+    imagem: "",
     itens: [
-      { nome: "Comercial Produção Padrão", preco: "R$ 50,00" },
-      { nome: "Comercial Produção Especial", preco: "R$ 70,00" },
-      { nome: "Comercial Porta de Loja", preco: "R$ 100,00" },
-      { nome: "Mídia para Ambiente Interno R$ 120,00", preco: "" },
-      { nome: "Vinhêtas para Rádios", preco: "R$ 50,00" },
-      { nome: "Jingles para Empresas", preco: "R$ 200,00" },
-      { nome: "Os valores estão sujeitos a alteração de preço!", preco: "" }
+      { nome: "Produto 1", preco: "R$ 10,00" },
+      { nome: "Produto 2", preco: "R$ 20,00" }
     ]
   },
 
