994307023bfe17fbba864450d65a63608689e66d
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2025-12-19 17:03:17 -0300

    Update personal and business information in data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
-window.data = {
+const data = {
   nome: "Jonathan Guilherme Oliveira",
   empresa: "CEO da Empresa",
   foto: "https://i.imgur.com/e1g5ycm.jpeg",
   whatsapp: "5588997842847",
-  email: "criativaaudiovisual@gmail.com",
+  email: "studiojonathanguilherme@gmail.com",
   site: "https://www.instagram.com/jonathanguilhermejg",
   pix: "88997842847",
   wifi: {
@@ -13,14 +13,16 @@ window.data = {
   },
 
   cardapio: {
-    estabelecimento: "Tag Motion",
-    imagem: "https://i.imgur.com/7noZxtR.png",
+    estabelecimento: "Studio JG",
+    imagem: "https://i.imgur.com/umvNas6.jpeg",
     itens: [
-      { nome: "Cartão de Visita Padrão", preco: "R$ 120,00" },
-      { nome: "Cartão de Visita + Avaliações", preco: "R$ 180,00" },
-      { nome: "Cardápio com Aproximação", preco: "200,00" },
-      { nome: "COMBO PREMIUM", preco: "" },
-      { nome: "CARTÃO", preco: "GOOGLE AVALIAÇÕES" }
+      { nome: "Comercial Produção Padrão", preco: "R$ 50,00" },
+      { nome: "Comercial Produção Especial", preco: "R$ 70,00" },
+      { nome: "Comercial Porta de Loja", preco: "R$ 100,00" },
+      { nome: "Mídia para Ambiente Interno R$ 120,00", preco: "" },
+      { nome: "Vinhêtas para Rádios", preco: "R$ 50,00" },
+      { nome: "Jingles para Empresas", preco: "R$ 200,00" },
+      { nome: "Os valores estão sujeitos a alteração de preço!", preco: "" }
     ]
   },
 
