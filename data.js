64f6557aba9bf527bcbdd206e33b4b1088c4f397
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2026-01-21 14:41:29 -0300

    Add JSON data for barber profile and services

--- data.js
+++ data.js
@@ -1,1 +1,59 @@
+{
+  "nome": "João Barber",
+  "profissao": "Barbeiro",
+  "fotoPerfil": "foto-perfil.jpg",
+
+  "contato": {
+    "telefone": "5511999999999",
+    "email": "joao@email.com"
+  },
+
+  "acoes": {
+    "salvarContato": true,
+    "copiarPix": true,
+    "copiarWifi": true
+  },
+
+  "links": [
+    {
+      "tipo": "whatsapp",
+      "label": "Falar no WhatsApp",
+      "url": "https://wa.me/5511999999999"
+    },
+    {
+      "tipo": "instagram",
+      "label": "Instagram",
+      "url": "https://instagram.com/joaobarber"
+    },
+    {
+      "tipo": "site",
+      "label": "Site",
+      "url": "https://joaobarber.com"
+    }
+  ],
+
+  "pix": {
+    "chave": "joao@email.com",
+    "tipo": "email"
+  },
+
+  "wifi": {
+    "nome": "Barbearia_WiFi",
+    "senha": "12345678"
+  },
+
+  "galeria": [
+    "foto1.jpg",
+    "foto2.jpg",
+    "foto3.jpg",
+    "foto4.jpg",
+    "foto5.jpg",
+    "foto6.jpg"
+  ],
+
+  "google": {
+    "maps": "https://maps.google.com/?q=Barbearia+Joao",
+    "avaliacoes": "https://search.google.com/local/reviews?placeid=XXXX"
+  }
+}
 
