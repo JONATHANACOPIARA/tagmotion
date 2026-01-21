1b7276ddd6697fda4b5988fef73923df98070676
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2026-01-21 14:50:46 -0300

    Update contact information and profile details

--- data.js
+++ data.js
@@ -1,59 +1,21 @@
 {
-  "nome": "João Barber",
-  "profissao": "Barbeiro",
-  "fotoPerfil": "foto-perfil.jpg",
-
+  "nome": "EDMUNDO DA VILA",
+  "profissao": "BARBEIRO DE PELA SACO",
   "contato": {
-    "telefone": "5511999999999",
-    "email": "joao@email.com"
+    "telefone": "5588997878787"
   },
-
-  "acoes": {
-    "salvarContato": true,
-    "copiarPix": true,
-    "copiarWifi": true
-  },
-
   "links": [
-    {
-      "tipo": "whatsapp",
-      "label": "Falar no WhatsApp",
-      "url": "https://wa.me/5511999999999"
-    },
     {
       "tipo": "instagram",
       "label": "Instagram",
-      "url": "https://instagram.com/joaobarber"
-    },
-    {
-      "tipo": "site",
-      "label": "Site",
-      "url": "https://joaobarber.com"
+      "url": "https://www.instagram.com/stories/deyvid_pianco"
     }
   ],
-
   "pix": {
-    "chave": "joao@email.com",
-    "tipo": "email"
+    "chave": "88997842847"
   },
-
   "wifi": {
-    "nome": "Barbearia_WiFi",
-    "senha": "12345678"
-  },
-
-  "galeria": [
-    "foto1.jpg",
-    "foto2.jpg",
-    "foto3.jpg",
-    "foto4.jpg",
-    "foto5.jpg",
-    "foto6.jpg"
-  ],
-
-  "google": {
-    "maps": "https://maps.google.com/?q=Barbearia+Joao",
-    "avaliacoes": "https://search.google.com/local/reviews?placeid=XXXX"
+    "nome": "BRISATESTE",
+    "senha": "ugxxsgtt46"
   }
 }
-
